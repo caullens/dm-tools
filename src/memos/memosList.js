04e5a3325f64206ae911646c400269588df2965c
Caullen Sasnett <caullens@Bitbucket.org>
2020-04-26 17:55:22 -0500

    Fix styling

---
+++ src/memos/memosList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {List, ListItem, ListItemText, makeStyles, Typography} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  list: {
+    height: '450px',
+    overflowY: 'auto'
+  },
+  noMemoText: {
+    fontStyle: 'italic',
+    color: theme.palette.text.secondary
+  }
+}));
+
+function MemosList({memos}) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      {!(memos && memos.length) && <Typography align="center" className={classes.noMemoText}>No memos</Typography>}
+      {memos && memos.map(memo => (
+        <ListItem divider key={memo}>
+          <ListItemText>{memo}</ListItemText>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
+
+export default MemosList;
